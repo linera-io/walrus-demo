7d054890d7719e251cb0b6180096f544e9439a67
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2025-02-18 21:53:11 +0000

    Implement reading of a Walrus blob in the service
    Send an HTTP request to a Walrus aggregator and return the blob bytes.

--- src/service.rs
+++ src/service.rs
@@ -7,8 +7,8 @@ mod state;
 
 use std::sync::{Arc, Mutex};
 
-use async_graphql::{connection::EmptyFields, EmptyMutation, EmptySubscription, Schema};
-use linera_sdk::{base::WithServiceAbi, Service, ServiceRuntime};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use linera_sdk::{base::WithServiceAbi, ensure, http, Service, ServiceRuntime};
 
 #[derive(Clone)]
 pub struct ApplicationService {
@@ -31,9 +31,49 @@ impl Service for ApplicationService {
     }
 
     async fn handle_query(&self, query: Self::Query) -> Self::QueryResponse {
-        Schema::build(EmptyFields, EmptyMutation, EmptySubscription)
-            .finish()
-            .execute(query)
-            .await
+        Schema::build(
+            Query {
+                runtime: self.runtime.clone(),
+            },
+            EmptyMutation,
+            EmptySubscription,
+        )
+        .finish()
+        .execute(query)
+        .await
+    }
+}
+
+/// Root type that defines all the GraphQL queries available from the service.
+pub struct Query {
+    runtime: Arc<Mutex<ServiceRuntime<ApplicationService>>>,
+}
+
+#[async_graphql::Object]
+impl Query {
+    /// Reads a blob from Walrus.
+    async fn read_blob(
+        &self,
+        aggregator_url: String,
+        blob_id: String,
+    ) -> async_graphql::Result<Vec<u8>> {
+        let mut runtime = self
+            .runtime
+            .lock()
+            .expect("Locking should never fail because service runs in a single thread");
+
+        let response = runtime.http_request(http::Request::get(format!(
+            "{aggregator_url}/v1/blobs/{blob_id}"
+        )));
+
+        ensure!(
+            response.status == 200,
+            async_graphql::Error::new(format!(
+                "Failed to read blob. Status code: {}",
+                response.status
+            ))
+        );
+
+        Ok(response.body)
     }
 }
