424a0252de22bb46224c57d3f8601da420171823
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2025-02-21 02:50:06 +0000

    Define an `Operation` type
    Add an operation to check a blob from Walrus.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use linera_sdk::base::{ContractAbi, ServiceAbi};
+use serde::{Deserialize, Serialize};
 
 pub struct ApplicationAbi;
 
 impl ContractAbi for ApplicationAbi {
-    type Operation = ();
+    type Operation = Operation;
     type Response = ();
 }
 
 impl ServiceAbi for ApplicationAbi {
     type Query = async_graphql::Request;
     type QueryResponse = async_graphql::Response;
+}
+
+/// Operations that the contract accepts.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum Operation {
+    /// Checks if a blob with the `blob_id` is available on Walrus and if the SHA-256 hash of its
+    /// contents is `blob_hash`.
+    CheckBlob {
+        blob_id: String,
+        blob_hash: [u8; 32],
+    },
 }
